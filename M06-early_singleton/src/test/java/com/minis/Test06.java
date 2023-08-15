84171fb99034c8da310c8365fc8d78c0938c2d21
rong.chen <18649808198@163.com>
2023-08-15 22:37:16 +0800

    M06.early_singleton

--- M06-early_singleton/src/test/java/com/minis/Test06.java
+++ M06-early_singleton/src/test/java/com/minis/Test06.java
@@ -1,15 +1,16 @@
 package com.minis;
 
 
+import com.minis.cycle.AService;
 import com.minis.spring.beans.BeansException;
 import com.minis.spring.context.ClassPathXmlApplicationContext;
 
-public class Test05 {
+public class Test06 {
 
     public static void main(String[] args) throws BeansException {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
-        BaseService aService = (BaseService) ctx.getBean("baseservice");
-//        AService aService = (AService) ctx.getBean("aservice");
-        aService.sayHi();
+//        BaseService aService = (BaseService) ctx.getBean("baseservice");
+        AService aService = (AService) ctx.getBean("aservice");
+        aService.sayHello();
     }
 }
